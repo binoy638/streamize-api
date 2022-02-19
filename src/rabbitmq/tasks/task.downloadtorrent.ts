5751050b2471c9ffd1266b0bf29be67e9b63199e
binoy638 <binoy638@gmail.com>
2022-02-19 18:15:55 +0530

    added torrent meta event listener

--- src/rabbitmq/tasks/task.downloadtorrent.ts
+++ src/rabbitmq/tasks/task.downloadtorrent.ts
@@ -1,5 +1,7 @@
+/* eslint-disable unicorn/consistent-destructuring */
 import { Channel, ChannelWrapper } from 'amqp-connection-manager';
 import { ConsumeMessage } from 'amqplib';
+import { Torrent } from 'webtorrent';
 import { ITorrent, TorrentPath, IVideo, QueueName } from '../../@types';
 import { IConvertVideoMessageContent, IMoveFilesMessageContent } from '../../@types/message';
 import logger from '../../config/logger';
@@ -7,6 +9,71 @@ import client from '../../config/webtorrent';
 import { allowedExt, convertableExt, getFileOutputPath, getMessageContent } from '../../utils/misc';
 import { getTorrentBySlug, updateTorrentFileConvertable, updateTorrentInfo } from '../../utils/query';
 import { isFileConvertable } from '../../utils/videoConverter';
+
+const handleCompletedTorrent = async (
+  torrentInClient: Torrent,
+  downloadedTorrent: ITorrent,
+  publisherChannel: ChannelWrapper,
+  consumerChannel: Channel,
+  message: ConsumeMessage
+) => {
+  await updateTorrentInfo(downloadedTorrent._id, { status: 'done' });
+
+  await Promise.allSettled(
+    downloadedTorrent.files.map(file => {
+      return isFileConvertable(file).then(isConvertable => {
+        updateTorrentFileConvertable(downloadedTorrent._id, file.slug, isConvertable);
+      });
+    })
+  ).catch(error => {
+    logger.error(error);
+  });
+
+  const UpdatedTorrent = await getTorrentBySlug(downloadedTorrent.slug);
+  if (!UpdatedTorrent) {
+    logger.error(`torrent not found after updating convertable data ${downloadedTorrent.name}`);
+    consumerChannel.ack(message);
+    return;
+  }
+
+  const convertableVideoFiles = UpdatedTorrent.files.filter(file => file.isConvertable);
+  const nonConvertableVideoFiles = UpdatedTorrent.files.filter(file => !file.isConvertable);
+
+  if (convertableVideoFiles.length > 0) {
+    //* sending all convertable files to convert-video queue
+    convertableVideoFiles.map(file =>
+      publisherChannel.sendToQueue(
+        QueueName.CONVERT_VIDEO,
+        {
+          torrentID: downloadedTorrent._id,
+          ...file,
+        } as IConvertVideoMessageContent,
+        { persistent: true }
+      )
+    );
+  }
+  if (nonConvertableVideoFiles.length > 0) {
+    //* sending all nonconvertable files to file-move queue to move them to download folder
+    await Promise.all(
+      nonConvertableVideoFiles.map(file =>
+        publisherChannel.sendToQueue(
+          QueueName.FILE_MOVE,
+          {
+            src: file.path,
+            dest: getFileOutputPath(file.name, TorrentPath.DOWNLOAD),
+            torrentID: downloadedTorrent._id,
+            fileSlug: file.slug,
+          } as IMoveFilesMessageContent,
+          { persistent: true }
+        )
+      )
+    );
+  }
+
+  consumerChannel.ack(message);
+  logger.info(`${downloadedTorrent.name} torrent downloaded now deleting torrent`);
+  torrentInClient.destroy();
+};
 
 export const downloadTorrent =
   (channel: Channel, publisherChannel: ChannelWrapper) =>
@@ -22,6 +89,7 @@ export const downloadTorrent =
           logger.error(`Torrent error, ack msg: ' ${addedTorrent} `);
           channel.ack(message);
         });
+
         const videofiles = torrent.files
           .map(file => {
             const ext = file.name.split('.').pop() || '';
@@ -64,65 +132,13 @@ export const downloadTorrent =
             channel.ack(message);
             return;
           }
-
+          torrent.on('metadata', async () => {
+            if (torrent.progress === 1) {
+              await handleCompletedTorrent(torrent, addedTorrent, publisherChannel, channel, message);
+            }
+          });
           torrent.on('done', async () => {
-            await updateTorrentInfo(SavedTorrent._id, { status: 'done' });
-
-            await Promise.allSettled(
-              SavedTorrent.files.map(file => {
-                return isFileConvertable(file).then(isConvertable => {
-                  updateTorrentFileConvertable(SavedTorrent._id, file.slug, isConvertable);
-                });
-              })
-            ).catch(error => {
-              logger.error(error);
-            });
-
-            const UpdatedTorrent = await getTorrentBySlug(SavedTorrent.slug);
-            if (!UpdatedTorrent) {
-              logger.error(`torrent not found after updating convertable data ${SavedTorrent.name}`);
-              channel.ack(message);
-              return;
-            }
-
-            const convertableVideoFiles = UpdatedTorrent.files.filter(file => file.isConvertable);
-            const nonConvertableVideoFiles = UpdatedTorrent.files.filter(file => !file.isConvertable);
-
-            if (convertableVideoFiles.length > 0) {
-              //* sending all convertable files to convert-video queue
-              convertableVideoFiles.map(file =>
-                publisherChannel.sendToQueue(
-                  QueueName.CONVERT_VIDEO,
-                  {
-                    torrentID: SavedTorrent._id,
-                    ...file,
-                  } as IConvertVideoMessageContent,
-                  { persistent: true }
-                )
-              );
-            }
-            if (nonConvertableVideoFiles.length > 0) {
-              //* sending all nonconvertable files to file-move queue to move them to download folder
-              await Promise.all(
-                nonConvertableVideoFiles.map(file =>
-                  publisherChannel.sendToQueue(
-                    QueueName.FILE_MOVE,
-                    {
-                      src: file.path,
-                      dest: getFileOutputPath(file.name, TorrentPath.DOWNLOAD),
-                      torrentID: SavedTorrent._id,
-                      fileSlug: file.slug,
-                    } as IMoveFilesMessageContent,
-                    { persistent: true }
-                  )
-                )
-              );
-            }
-
-            channel.ack(message);
-            logger.info(`${SavedTorrent.name} torrent downloaded now deleting torrent`);
-            //! this might leave usless files in tmp folder
-            torrent.destroy();
+            await handleCompletedTorrent(torrent, SavedTorrent, publisherChannel, channel, message);
           });
         }
       });
