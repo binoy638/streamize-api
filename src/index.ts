eed75ab389abad4ad4b1f84c44a579f7908bbdc1
binoy638 <binoy638@gmail.com>
2022-06-12 21:12:42 +0530

    ci: fix

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ app.use(
   cookieSession({
     secret: process.env.COOKIE_SECRET!,
     maxAge: 24 * 60 * 60 * 1000 * 7,
-    // sameSite: 'none',
+    sameSite: 'none',
 
     //* https only cookies
     secure: process.env.NODE_ENV !== 'development',
