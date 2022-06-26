1d8af8d9c78b1ed12603ee46189b9dfa92568de0
binoy638 <binoy638@gmail.com>
2022-06-26 22:32:44 +0530

    feat:fetch torrent download info using graphql

--- src/graphql/resolvers/resolver.ts
+++ src/graphql/resolvers/resolver.ts
@@ -3,10 +3,12 @@
 /* eslint-disable max-classes-per-file */
 
 /* eslint-disable class-methods-use-this */
-import { Resolver, Query, Arg, InputType, Field, Ctx } from 'type-graphql';
-import { ITorrent, UserPayload } from '../../@types';
+import { Resolver, Query, Arg, InputType, Field, Ctx, Int } from 'type-graphql';
+import { ITorrent, TorrentState, UserPayload } from '../../@types';
+import client from '../../config/webtorrent';
 import { TorrentModel } from '../../models/torrent.schema';
 import { UserModel } from '../../models/user.schema';
+import { UserVideoProgressModel } from '../../models/userVideoProgress.schema';
 import Utils from '../../utils';
 import { DiskUsage, Torrent, Video } from '../typeDefs/typeDefs';
 
@@ -20,17 +22,46 @@ export class TorrentResolver {
       .populate<{ torrents: ITorrent[] }>('torrents')
       .lean();
     if (!userDoc) throw new Error('User not found');
-    return userDoc.torrents;
+
+    const { torrents } = userDoc;
+    const torrentsWithDownloadInfo = torrents.map(torrent => {
+      if (torrent.status === TorrentState.DOWNLOADING) {
+        const torrentInClient = client.get(torrent.infoHash);
+        if (torrentInClient) {
+          return { ...torrent, downloadInfo: Utils.getDataFromTorrent(torrentInClient) };
+        }
+        return torrent;
+      }
+      return torrent;
+    });
+    return torrentsWithDownloadInfo;
   }
 
   @Query(() => Torrent, { nullable: true })
   async torrent(@Arg('slug') slug: string, @Ctx() ctx: { user: UserPayload }) {
     if (!ctx.user) throw new Error('User not found');
-    const torrent = await TorrentModel.findOne({ slug }).lean();
-    if (!torrent) throw new Error('Torrent not found');
-    const haveAccess = await UserModel.find({ _id: ctx.user.id, torrents: { $in: [torrent._id] } });
+    const torrentDoc = await TorrentModel.findOne({ slug }).lean();
+    if (!torrentDoc) throw new Error('Torrent not found');
+    const haveAccess = await UserModel.find({ _id: ctx.user.id, torrents: { $in: [torrentDoc._id] } });
     if (!haveAccess) throw new Error('Unauthorized');
-    return torrent;
+    if (torrentDoc.status === TorrentState.DOWNLOADING) {
+      const torrent = client.get(torrentDoc.infoHash);
+      if (torrent) {
+        const downloadInfo = Utils.getDataFromTorrent(torrent);
+
+        const filesWithDownloadInfo = torrentDoc.files.map(docFile => {
+          const file = torrent.files.find(file => file.name === docFile.name);
+          if (file) {
+            const downloadInfo = Utils.getDataFromTorrentFile(file);
+            return { ...docFile, downloadInfo };
+          }
+          return docFile;
+        });
+        torrentDoc.downloadInfo = downloadInfo;
+        torrentDoc.files = filesWithDownloadInfo;
+      }
+    }
+    return torrentDoc;
   }
 }
 
@@ -45,7 +76,7 @@ class VideoInput {
 
 @Resolver()
 export class VideoResolver {
-  @Query(() => Video, { nullable: true })
+  @Query(() => Video)
   async video(@Arg('input') input: VideoInput, @Ctx() ctx: { user: UserPayload }) {
     if (!ctx.user) throw new Error('User not found');
     const torrent = await TorrentModel.findOne({ slug: input.torrentSlug }).lean();
@@ -55,7 +86,17 @@ export class VideoResolver {
 
     if (!haveAccess) throw new Error('Unauthorized');
 
-    return torrent.files.find(file => file.slug === input.videoSlug);
+    const video = torrent.files.find(file => file.slug === input.videoSlug);
+    if (!video) throw new Error('Video not found');
+    return video;
+  }
+
+  @Query(() => Int)
+  async videoProgress(@Arg('videoSlug') videoSlug: string, @Ctx() ctx: { user: UserPayload }) {
+    if (!ctx.user) throw new Error('User not found');
+    const doc = await UserVideoProgressModel.findOne({ user: ctx.user.id, video: videoSlug });
+    if (!doc) throw new Error('Video Progress not found');
+    return doc.progress;
   }
 }
 
