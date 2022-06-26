4357a090d43594ed7c39925c6eac466e542f16ad
binoy638 <binoy638@gmail.com>
2022-06-26 19:48:02 +0530

    feat: resolver support auth

--- src/graphql/resolvers/resolver.ts
+++ src/graphql/resolvers/resolver.ts
@@ -4,8 +4,9 @@
 
 /* eslint-disable class-methods-use-this */
 import { Resolver, Query, Arg, InputType, Field, Ctx } from 'type-graphql';
-import { UserPayload } from '../../@types';
+import { ITorrent, UserPayload } from '../../@types';
 import { TorrentModel } from '../../models/torrent.schema';
+import { UserModel } from '../../models/user.schema';
 import Utils from '../../utils';
 import { DiskUsage, Torrent, Video } from '../typeDefs/typeDefs';
 
@@ -13,15 +14,22 @@ import { DiskUsage, Torrent, Video } from '../typeDefs/typeDefs';
 export class TorrentResolver {
   @Query(() => [Torrent])
   //! pagination
-  async torrents() {
-    const torrents = await TorrentModel.find({}).lean();
-    return torrents;
+  async torrents(@Ctx() ctx: { user: UserPayload }) {
+    if (!ctx.user) throw new Error('User not found');
+    const userDoc = await UserModel.findOne({ __id: ctx.user.id })
+      .populate<{ torrents: ITorrent[] }>('torrents')
+      .lean();
+    if (!userDoc) throw new Error('User not found');
+    return userDoc.torrents;
   }
 
   @Query(() => Torrent, { nullable: true })
-  async torrent(@Arg('slug') slug: string) {
+  async torrent(@Arg('slug') slug: string, @Ctx() ctx: { user: UserPayload }) {
+    if (!ctx.user) throw new Error('User not found');
     const torrent = await TorrentModel.findOne({ slug }).lean();
     if (!torrent) throw new Error('Torrent not found');
+    const haveAccess = await UserModel.find({ _id: ctx.user.id, torrents: { $in: [torrent._id] } });
+    if (!haveAccess) throw new Error('Unauthorized');
     return torrent;
   }
 }
@@ -38,11 +46,16 @@ class VideoInput {
 @Resolver()
 export class VideoResolver {
   @Query(() => Video, { nullable: true })
-  async video(@Arg('input') input: VideoInput) {
-    const video = await TorrentModel.findOne({ slug: input.torrentSlug }).lean();
-    if (!video) throw new Error('Video not found');
+  async video(@Arg('input') input: VideoInput, @Ctx() ctx: { user: UserPayload }) {
+    if (!ctx.user) throw new Error('User not found');
+    const torrent = await TorrentModel.findOne({ slug: input.torrentSlug }).lean();
+    if (!torrent) throw new Error('Video not found');
 
-    return video.files.find(file => file.slug === input.videoSlug);
+    const haveAccess = await UserModel.find({ _id: ctx.user.id, torrents: { $in: [torrent._id] } });
+
+    if (!haveAccess) throw new Error('Unauthorized');
+
+    return torrent.files.find(file => file.slug === input.videoSlug);
   }
 }
 
