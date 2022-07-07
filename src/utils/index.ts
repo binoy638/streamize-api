b80e1a2be8f62b553b8bd968ac4675986c6eaade
binoy638 <binoy638@gmail.com>
2022-07-07 19:46:37 +0530

    fix: time remaining is null

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -32,7 +32,7 @@ class Utils {
       downloadSpeed: torrent.downloadSpeed,
       uploadSpeed: torrent.uploadSpeed,
       progress: torrent.progress,
-      timeRemaining: torrent.timeRemaining ? torrent.timeRemaining : 0,
+      timeRemaining: torrent.timeRemaining === null ? 0 : torrent.timeRemaining,
       paused: torrent.paused,
       completed: torrent.done,
     };
